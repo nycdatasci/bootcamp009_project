58c0648c9b677397d93c02e79a215a0183497535
Rachel Kogan <rachelkogan@Rachels-MacBook-Air.local>
2017-04-21 21:40:56 -0400

    adding rachels app

--- Project1-ExploreVis/rachelshiny/server.R
+++ Project1-ExploreVis/rachelshiny/server.R
@@ -1,15 +1,28 @@
 library(DT)
 library(ggplot2)
+library(ggvis)
+library(leaflet)
 library(dplyr)
 ## server.R ##
 shinyServer(function(input, output, session){
   output$controlPlot <- renderPlot({
-    ggplot(data=data_1(), aes(x=x_var, y=y_var)) + geom_boxplot()
+    ggplot(data=data_1(), aes(x=x_var, y=y_var)) + geom_violin()
   })
   
-  output$statePlot <- renderPlot({
-    leaflet() %>% addPolygons(data=state_map_info(), fillColor='red') %>% 
-      addCircles(lng=data_state_lng(), lat=data_state_lat(), color = 'green', popup='da')
+  output$graddensityPlot <- renderPlot({
+    ggplot(data=data_1(), aes(x=y_var)) + geom_density() + xlim(0, 1)
+  })
+  
+  output$statePlot <- renderLeaflet({
+    leaflet() %>% addTiles() %>%
+      addPolygons(data=state_map_info(), weight = 1) %>%
+      addMarkers(data=state_school(), lng=~LONGITUDE, lat=~LATITUDE, 
+                 clusterOptions = markerClusterOptions(),
+                 popup=~ paste('<b><font color="Red">', INSTNM, '</font></b><br/>', 
+                               'acceptance rate: ', ADM_RATE, '<br/>',
+                               'undergrads: ', UGDS, '<br/>',
+                               '4 year graduation rate: ', C150_4, '<br/>', 
+                               'median earnings: ', MN_EARN_WNE_P7, '<br/>'))
   })
   
   data_1 <- reactive({
@@ -23,19 +36,89 @@ shinyServer(function(input, output, session){
     }
   })
   
-  data_state_lng <- reactive({
-    data %>% filter(STATE==input$state_choice) %>% select(LONGITUDE)
+  
+  
+  state_school <- reactive({
+    data %>% filter(STABBR==input$state_choice)
   })
   
-  data_state_lat <- reactive({
-    data %>% filter(STATE==input$state_choice) %>% select(LATITUDE)
+  region_school <- reactive({
+    data %>% filter(REGION_2==input$region_choice)
   })
+  
+  data_scatter <- reactive({
+    # Due to dplyr issue #318, we need temp variables for input values
+    ugrads <- input$ugrads
+    highest_deg <- input$highest_deg
+    
+    # Apply filters
+    ds <- data %>%
+      filter(
+        UGDS >= ugrads
+      ) %>%
+      arrange(UNITID)
+    
+    if(highest_deg!='All') {
+      ds <- ds %>% filter(HIGHDEG==highest_degree[[highest_deg]])
+    }
+    
+    
+    if(input$highest_deg == "all"){
+      map("state", plot=F, fill = T)
+    }else{
+    }
+    
+    # Optional: filter by college name
+    if (!is.null(input$collegeName) && input$collegeName != "") {
+      college_name <- paste0(input$collegeName, "")
+      ds <- ds[grep(pattern=input$collegeName, x=ds$INSTNM),]
+    }
+
+    ds <- as.data.frame(ds)
+    
+    # Add column which says whether the movie won any Oscars
+    # Be a little careful in case we have a zero-row data frame
+    ds
+  })
+  
+  vis <- reactive({
+    # Lables for axes
+    xvar_name <- names(x_vars)[x_vars == input$xvar]
+    yvar_name <- names(y_vars)[y_vars == input$yvar]
+    
+    # Normally we could do something like props(x = ~BoxOffice, y = ~Reviews),
+    # but since the inputs are strings, we need to do a little more work.
+    xvar <- prop("x", as.symbol(input$xvar))
+    yvar <- prop("y", as.symbol(input$yvar))
+    
+    data_scatter %>%
+      ggvis(x = xvar, y = yvar) %>%
+      layer_points(size := 50, size.hover := 200, stroke:='blue',
+                   fillOpacity := 0.2, fillOpacity.hover := 0.5) %>%
+      #add_tooltip(movie_tooltip, "hover") %>%
+      add_axis("x", title = xvar_name) %>%
+      add_axis("y", title = yvar_name) %>%
+      layer_smooths() %>%
+      #add_legend("stroke", title = "Won Oscar", values = c("Yes", "No")) %>%
+      scale_nominal("stroke", domain = c("Yes", "No"),
+                    range = c("orange", "#aaa"))
+  })
+  
+  vis %>% bind_shiny("plot1")
+  
   
   state_map_info <- reactive({
     state_name <- tolower(state.name[which(state.abb==input$state_choice)])
-    map("state", region=state_name, plot=T, col=, fill = T)
+    if(input$state_choice == "all"){
+      map("state", plot=F, fill = T)
+    }else{
+      map("state", region=state_name, plot=F, col=, fill = T)
+    }
   })
   
+  output$table <- DT::renderDataTable({
+    datatable(data=data, rownames=FALSE) 
+  })
   
   
   
@@ -45,5 +128,3 @@ shinyServer(function(input, output, session){
   #     choices = choice)
   # })
 })
-
-
--- Project1-ExploreVis/rachelshiny/ui.R
+++ Project1-ExploreVis/rachelshiny/ui.R
@@ -1,23 +1,23 @@
 ## ui.R ##
 library(shinydashboard)
+library(leaflet)
+library(maps)
 
 shinyUI(dashboardPage(
-  dashboardHeader(title = "My Dashboard"),
+  dashboardHeader(title = "College Comparison Dashboard", titleWidth = 350),
   dashboardSidebar(
-    sidebarUserPanel("NYC DSA",
+    sidebarUserPanel("Tabs",
                      image = 'https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/567/s300/data-science-logos-final.jpg'
     ),
     sidebarMenu(
-      menuItem("Map", tabName = "map", icon = icon("map")),
+      menuItem("By Location", tabName = "map", icon = icon("map")),
       menuItem("Charts", tabName = "ch", icon = icon("chart")),
+      menuItem("Scatter Plots", tabName = "sc", icon = icon("chart")),
       menuItem("Data", tabName = "data", icon = icon("database"))),
     
     selectizeInput(inputId="deg_length",
                    label="Select Degree Length",
-                   choices=choice),
-    selectizeInput(inputId="state_choice",
-                   label="Select State",
-                   choices=state_vector)
+                   choices=choice)
   ),
   dashboardBody(
     tabItems(
@@ -26,16 +26,60 @@ shinyUI(dashboardPage(
               #          infoBoxOutput("minBox"),
               #          infoBoxOutput("avgBox")),
               # gvisGeoChart
-              fluidRow(box(plotOutput("statePlot"), height=300))
+              fluidRow(box(
+                selectizeInput(inputId="region_choice",
+                               label=h4("Select Region"),
+                               selected=NULL,
+                               choices=region_vector)),
+                box(
+                selectizeInput(inputId="state_choice",
+                             label=h4("Select State"),
+                             selected="all",
+                             choices=state_vector)) 
+                ),
+              fluidRow(box(leafletOutput("statePlot"), height=420, width=200)),
+              fluidRow()
               #          # gvisHistoGram
               #          box(htmlOutput("hist"), height=300))),
       ),
       tabItem(tabName = 'ch',
-              fluidRow(box(plotOutput('controlPlot')))
+              fluidRow(box(plotOutput('controlPlot'))),
+              fluidRow(box(plotOutput('graddensityPlot')))
       ),
-      tabItem(tabName = "data"
+      tabItem(tabName = 'sc', 
+              fluidRow(
+                column(4,
+                       wellPanel(
+                         h4("Filter"),
+                         sliderInput("ugrads", "Minimum number of undergraduate students",
+                                     0, 30000, 50, step = 100),
+                         selectInput(inputId="highest_deg",
+                                    label="Highest Degree Awarded",
+                                    choices=names(highest_degree),
+                                    selected = 'All'),
+                         textInput("collegeName", "College name contains (e.g., Nursing)")
+                       ),
+                       wellPanel(
+                         selectInput(inputId='xvar', 
+                                     label="X-axis variable",
+                                     choices= x_vars),
+                         selectInput("yvar", "Y-axis variable", 
+                                     choices = y_vars, 
+                                     selected = 'MN_EARN_WNE_P7'),
+                         tags$small(paste0(
+                           "Note: The Tomato Meter is the proportion of positive reviews",
+                           " (as judged by the Rotten Tomatoes staff), and the Numeric rating is",
+                           " a normalized 1-10 score of those reviews which have star ratings",
+                           " (for example, 3 out of 4 stars)."
+                         ))
+                       )
+                ),
+                column(8, ggvisOutput("plot1")))
+              
+            ),
+      tabItem(tabName = "data",
               # datatable
-              # fluidRow(box(DT::dataTableOutput("table"))))
+              fluidRow(box(DT::dataTableOutput("table")))
       )
-  ))
+    ))
 ))
