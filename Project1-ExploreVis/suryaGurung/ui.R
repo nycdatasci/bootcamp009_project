48e9cf51a67e3f40529641c6297749b814a3422a
slgurung <slg419@nyu.edu>
2017-04-23 17:01:34 -0400

    second commit 5:01

--- Project1-ExploreVis/suryaGurung/ui.R
+++ Project1-ExploreVis/suryaGurung/ui.R
@@ -26,46 +26,59 @@ shinyUI(
         ),
         dashboardBody(
             fluidRow(
-                box(
-                    h4("Bankruptcy Predictor:"),
-                    width = 12, background = "light-blue"
+                column(
+                    img(src = "chart.jpg",height = 80, width = "100%"),
+                    width = 12
+                    #background = "light-blue"
                     #strong("Bankruptcy Predictor")
                 )
             ),
+            fluidRow(box(width=12, height = "0px")),
             fluidRow(
                 
                 tabBox(
                     #title = "Surya Gurung",
                     # The id lets us use input$tabset1 on the server to find the current tab
-                    id = "tabset1", height = "550px",
+                    id = "tabset1", height = "500px",
                     width = 9, 
-                    tabPanel("Z-Score",
-                        plotlyOutput("plot")
+                    tabPanel("Z-Score: (NYSE)",
+                        plotlyOutput("all")
                     ),
-                    tabPanel("Features")
+                    tabPanel("Industries", 
+                             plotlyOutput("sector")         
+                    ),
+                    tabPanel("Company", 
+                        plotlyOutput("company")         
+                    ),
+                    tabPanel("Features", 
+                             plotlyOutput("features")         
+                    )
                 ),
                 box(
                     #title = h4("Bankruptcy Predictor"),
                     background = 'aqua',
                     width = 3, 
-                    height = "550px",
+                    height = "500px",
                     #dateRangeInput("dates", 
                     #               "Date Range",
                     #               start = "2013-01-01", 
                     #               end = as.character(Sys.Date())),
-                    selectizeInput('selectSector', label = h4('Sector'),
-                                   choices = fundamentals$Sector, selected = 'Industrials'
+                    selectizeInput('selectSector', label = h4('Industries'),
+                                   choices = fundamentals$Sector, selected = ''
+                    ),
+                    
+                    selectizeInput('selectCompany', label = h4('Symbol'),
+                                   choices = unique(fundamentals$Ticker), selected = ''
                     ),
                     hr(),
-                    sidebarSearchForm(textId = "tickerSearch", buttonId = "searchButton",
-                                      label = "Ticker Search")
-                    
+                    br(),
+                    br()
                     
                 )
             ),
             fluidRow(
-                box(width = 12, background = 'light-blue',
-                strong("by: Surya Gurung")    
+                box(width = 12, background = 'blue',
+                    strong("by: Surya Gurung")    
                 )
             )
         )
