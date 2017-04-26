1a78505ba67f84e65de87befbdd6b35bfc428ba2
jcenyeart <jcenyeart@users.noreply.github.com>
2017-04-26 14:53:33 -0400

    project v2

--- Project1-ExploreVis/jackenyeart/global.R
+++ Project1-ExploreVis/jackenyeart/global.R
@@ -9,7 +9,7 @@ library(tidyr)
 library(plotly)
 
 topshooters = read.csv("topshooters.csv", stringsAsFactors = FALSE)[,2:6]
-
+trimmed1 = read.csv("trimmed1.csv", stringsAsFactors = FALSE)
 items = c("SHOT_DIST","CLOSE_DEF_DIST","SHOOTER_height")
 items2 = names(topshooters)
 
