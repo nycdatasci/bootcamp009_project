7bb0bd383724fff59bc2dc7e61f58432099397e0
Kai <kaichan91@gmail.com>
2017-04-25 20:12:27 -0400

    changes

--- Project1-ExploreVis/kaichan/server.R
+++ Project1-ExploreVis/kaichan/server.R
@@ -3,64 +3,66 @@ library(shiny)
 
 
 shinyServer(function(input, output){
-
-  ##Graphs
-  output$densityAT <- renderPlot(
+  ##Text
+  
+  ##Reactive Functions
+  
+  ##Static Graphs
+  output$densityAT <- renderPlot({
     a_tov %>%
     ggplot(aes(x=ast/tov)) +
     geom_density(colour = 'red', fill = 'white') + 
     facet_grid(~conf_finals) +
     ggtitle("Assist to Turnover by C.F. Status")
-    )
+    })
   
-  output$densityshotprop <- renderPlot(
+  output$densityshotprop <- renderPlot({
     fga_3pa %>%
     ggplot(aes(x=fg3a/fga)) +
     geom_density(colour = 'blue',fill='white')+
     facet_grid(~conf_finals) +
     ggtitle("3-Point Attempts in Proportion to Overall FGs")
-    )
+    })
     
   
-  output$densityftpct <- renderPlot(
+  output$densityftpct <- renderPlot({
     freethrows %>%
     ggplot(aes(x=ftacc, y=ft)) +
     geom_point(colour = 'orange') + 
     facet_grid(~conf_finals) +
     ggtitle("Free Throw Percentage to Free Throw Attempts")
-    )
+    })
   
-  output$densitypts <- renderPlot(
+  output$densitypts <- renderPlot({
     avg_pts %>%
     ggplot(aes(x=avgpts)) +
     geom_density(colour = 'green', fill='white') +
     facet_grid(~conf_finals) +
     ggtitle("Average Points Scored")
-    )
+    })
     
-  output$densitydrebs <- renderPlot(
+  output$densitydrebs <- renderPlot({
     drebs %>%
     ggplot(aes(x=rebound)) +
     geom_density(colour = 'red2', fill = 'white') +
     facet_grid(~conf_finals) +
     ggtitle("Average Defensive Rebounds")
-    )
+    })
   
-  output$densitypminus <- renderPlot(
+  output$densitypminus <- renderPlot({
     plus_minus %>%
       ggplot(aes(x=pminus)) +
       geom_density(colour='blue2', fill = 'white') +
       facet_grid(~conf_finals) +
       ggtitle("Plus Minus")
-    )
+    })
   
-  output$densityfouls <- renderPlot(
+  output$densityfouls <- renderPlot({
     fouls %>%
       ggplot(aes(x=fouls)) +
       geom_density(fill='white') +
       facet_grid(~conf_finals) +
       ggtitle("Average Fouls")
-  )
+    })
   
-}
-)
+})
--- Project1-ExploreVis/kaichan/ui.R
+++ Project1-ExploreVis/kaichan/ui.R
@@ -3,40 +3,54 @@ library(shiny)
 library(shinydashboard)
 
 shinyUI(dashboardPage(
-  dashboardHeader(title = "EDA: NBA Regular Season"),
+  
+  #Header
+  dashboardHeader(title = "Conference Finals Teams in the NBA Regular Season"),
+  
+  #Sidebar
   dashboardSidebar(
     
-    sidebarUserPanel("NYC DSA",
+    sidebarUserPanel("Kai Chan",
                      image = "https://yt3.ggpht.com/-04uuTMHfDz4/AAAAAAAAAAI/AAAAAAAAAAA/Kjeupp-eNNg/s100-c-k-no-rj-c0xffffff/photo.jpg"),
     sidebarMenu(
-      menuItem("Main", text = 'Please select a graph in the submenu below.'),
-      menuItem("Graphs", tabName = 'graphs',
-        menuSubItem("Assists to Turnover", tabName = 'atov'),
-        menuSubItem("Field Goals to 3 Point Attempts", tabName = 'fg3p'),
-        menuSubItem("Free Throw Percentage", tabName = 'freethrows'),
-        menuSubItem("Average Points Scored", tabName = 'avg_pts'),
-        menuSubItem("Average Defensive Rebounds", tabName = 'drebs'),
-        menuSubItem("Plus Minus", tabName = 'pminus'),
-        menuSubItem("Density Fouls", tabName = 'fouls'))
-    
+      menuItem("Main", icon = icon("info"), tabName = "main"),
+      menuItem("Graphs", icon = icon("line-chart"), tabName = "graphs",
+        menuSubItem("Offense", tabName = "offense"),
+        menuSubItem("Defense", tabName = "defense"),
+        menuSubItem("Other", tabName = "other"))
+      )
     ),
+  
+  #Body
   dashboardBody(
+    
+      
     tabItems(
-      tabItem(tabName = 'atov',
-              plotOutput('densityAT')),
-      tabItem(tabName = 'fg3p',
-              plotOutput('densityshotprop')),
-      tabItem(tabName = 'freethrows',
-              plotOutput('densityftpct')),
-      tabItem(tabName = 'avg_pts',
-              plotOutput('densitypts')),
-      tabItem(tabName = 'drebs',
-              plotOutput('densitydrebs')),
-      tabItem(tabName = 'pminus',
-              plotOutput('densitypminus')),
-      tabItem(tabName = 'fouls',
-              plotOutput('densityfouls'))
+      tabItem
+             (tabName = "main", "Welcome!"),
+      
+      tabItem(tabName = "offense",
               
-    )
-  )
-)))
+              fluidRow(
+                box(plotOutput("densityAT")),
+                box(plotOutput("densityshotprop")),
+                box(plotOutput("densityftpct")),
+                box(plotOutput("densitypts"))
+                
+              )
+      ),
+      
+      tabItem(tabName = "defense",
+              plotOutput("densitydrebs"),
+              plotOutput("densitydrebs"),
+              plotOutput("densityfouls")),
+      
+      tabItem(tabName = "other",
+              plotOutput("densitypminus"))
+              
+      
+    ) #tabItems close
+      
+    
+  ) #dashboardBody close
+)) #ShinyUI/dashboardPage Close
