78efcaf69802b21b36c43d23d24f78e358b63c2e
Economic(a) <editor@economica.org>
2017-05-01 21:31:39 -0400

    v4.1 but with broken sqlite export using sqlachemy / prior to implementing session

--- Project2-WebScraping/WesAull/gcor/pipelines.py
+++ Project2-WebScraping/WesAull/gcor/pipelines.py
@@ -1,7 +1,23 @@
 from scrapy.exceptions import DropItem
 import sqlite3
 from os import path
-sqlite_file = '/Users/WesAull/GoogleDrive/NYCDSA/bootcamp009_project/Project2-WebScraping/WesAull/IBD.db'
+from sqlalchemy import *
+from datetime import datetime
+engine = create_engine("sqlite:////Users/WesAull/GoogleDrive/NYCDSA/bootcamp009_project/Project2-WebScraping/WesAull/IBD.db")
+metadata = MetaData()
+search = Table('search', metadata,
+    Column('search_term', String(25), primary_key=True),
+    Column('search_activity', Float, nullable = False),
+    Column('date', Date, nullable = False)
+    )
+
+correlation = Table('correlation', metadata,
+    Column('search_term', String(25), ForeignKey("search.search_term"), primary_key = True),
+    Column('corr_term', String(25), nullable = False),
+    Column('corr', Float, nullable = False)
+    )
+
+metadata.create_all(engine)
 
 class ValidateItemPipeline(object):
 
@@ -14,42 +30,21 @@ class ValidateItemPipeline(object):
 class WriteItemSQLitePipeline(object):
     def __init__(self):
         self.setupDBCon()
-        self.createTables()
 
     def setupDBCon(self):
-        self.conn = sqlite3.connect(sqlite_file)
-        self.cur = self.conn.cursor()
-
-    def createTables(self):
-        self.dropIBDTable()
-        self.createIBDTable()
-
-    def createIBDTable(self):
-        self.cur.execute("CREATE TABLE IF NOT EXISTS IBD(id INTEGER PRIMARY KEY NOT NULL, \
-            name TEXT, \
-            path TEXT, \
-            source TEXT \
-            )")
-
-    def dropIBDTable(self):
-        self.cur.execute("DROP TABLE IF EXISTS IBD")
+        self.conn = engine.connect()
 
     def process_item(self, item, spider):
         self.storeInDb(item)        
         return item
 
     def storeInDb(self,item):
-        self.cur.execute("INSERT INTO IBD(\
-            name, \
-            path, \
-            source \
-            ) \
-            VALUES( ?, ?, ?)", \
-            ( \
-                item.get('Name',''),
-                item.get('Path',''),
-                item.get('Source','')
-            ))
+        for i in item['hist_search_activity']:
+            engine.execute(search.insert(), search_term=item['search_term'], date=datetime.strptime(i['date'],'%Y-%m-%d').date(), search_activity=i['value'])
+        
+        corr = zip(item['corr_terms'],item['corr_terms_cor'])
+        for i,j in corr:
+            engine.execute(correlation.insert(), search_term=item['search_term'], corr_term=i, corr=j)
         print '------------------------'
         print 'Data Stored in Database'
         print '------------------------'
