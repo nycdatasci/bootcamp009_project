cce360e52c3b60d7ce5ada882d8f2f240758d852
Economic(a) <editor@economica.org>
2017-05-02 13:15:00 -0400

    v4.2 -- about to give up on SQLAlchemy

--- Project2-WebScraping/WesAull/gcor/pipelines.py
+++ Project2-WebScraping/WesAull/gcor/pipelines.py
@@ -1,23 +1,12 @@
 from scrapy.exceptions import DropItem
-import sqlite3
-from os import path
-from sqlalchemy import *
+import os
+import sys
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Float, Date, MetaData
+from sqlalchemy import create_engine
 from datetime import datetime
-engine = create_engine("sqlite:////Users/WesAull/GoogleDrive/NYCDSA/bootcamp009_project/Project2-WebScraping/WesAull/IBD.db")
-metadata = MetaData()
-search = Table('search', metadata,
-    Column('search_term', String(25), primary_key=True),
-    Column('search_activity', Float, nullable = False),
-    Column('date', Date, nullable = False)
-    )
 
-correlation = Table('correlation', metadata,
-    Column('search_term', String(25), ForeignKey("search.search_term"), primary_key = True),
-    Column('corr_term', String(25), nullable = False),
-    Column('corr', Float, nullable = False)
-    )
+sqlite_file = "sqlite:////Users/WesAull/GoogleDrive/NYCDSA/bootcamp009_project/Project2-WebScraping/WesAull/IBD.db"
 
-metadata.create_all(engine)
 
 class ValidateItemPipeline(object):
 
@@ -29,26 +18,41 @@ class ValidateItemPipeline(object):
 
 class WriteItemSQLitePipeline(object):
     def __init__(self):
-        self.setupDBCon()
+        self.OpenDb()
 
-    def setupDBCon(self):
-        self.conn = engine.connect()
+    def OpenDb():
+        engine = create_engine("sqlite:////Users/WesAull/GoogleDrive/NYCDSA/bootcamp009_project/Project2-WebScraping/WesAull/IBD.db", echo=True)
+        metadata = MetaData()
+        search = Table('search', metadata,
+            Column('search_term', String(25), primary_key=True),
+            Column('search_activity', Float, nullable = False),
+            Column('date', Date, nullable = False)
+            )
+
+        correlation = Table('correlation', metadata,
+            Column('search_term', String(25), ForeignKey("search.search_term"), primary_key = True),
+            Column('corr_term', String(25), nullable = False),
+            Column('corr', Float, nullable = False)
+            )
+        metadata.create_all(engine)
 
     def process_item(self, item, spider):
-        self.storeInDb(item)        
+        self.storeInDb(item)
         return item
 
     def storeInDb(self,item):
         for i in item['hist_search_activity']:
-            engine.execute(search.insert(), search_term=item['search_term'], date=datetime.strptime(i['date'],'%Y-%m-%d').date(), search_activity=i['value'])
+            conn.execute(search.insert(), search_term=item['search_term'], date=datetime.strptime(i['date'],'%Y-%m-%d').date(), search_activity=i['value'])
         
         corr = zip(item['corr_terms'],item['corr_terms_cor'])
         for i,j in corr:
-            engine.execute(correlation.insert(), search_term=item['search_term'], corr_term=i, corr=j)
+            conn.execute(correlation.insert(), search_term=item['search_term'], corr_term=i, corr=j)
         print '------------------------'
         print 'Data Stored in Database'
         print '------------------------'
-        self.conn.commit()
+        print '------------------------'
+        print 'Data Stored in Database'
+        print '------------------------'
 
     def closeDB(self):
         self.conn.close()
