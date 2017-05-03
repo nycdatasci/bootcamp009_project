8ba1eeb53bdd12067811bb857cd8f72750387d90
Bo Kim <bosang@gmail.com>
2017-05-03 16:45:26 -0400

    0504

--- Project2-WebScraping/bo_k/yahoofinance/spiders/yahoofinance_spider.py
+++ Project2-WebScraping/bo_k/yahoofinance/spiders/yahoofinance_spider.py
@@ -26,17 +26,16 @@ class yahoofinance_spider(scrapy.Spider):
 
         for i in range(1, len(rows)):
 
-
-            strike =       rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[1]//text()').extract_first()
-            contractname = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[2]//text()').extract_first()
-            lastprice = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[3]//text()').extract_first()
-            bid = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[4]//text()').extract_first()
-            ask = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[5]//text()').extract_first()
-            change = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[6]//text()').extract_first()
-            changeperc = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[7]//text()').extract_first()
-            volumn = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[8]//text()').extract_first()
-            openinterest = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[9]//text()').extract_first()
-            impliedvolatility = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[1]/td[10]//text()').extract_first()
+            strike =       rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[1]//text()').extract_first()
+            contractname = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr['+ str(i) + ']/td[2]//text()').extract_first()
+            lastprice = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[3]//text()').extract_first()
+            bid = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[4]//text()').extract_first()
+            ask = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[5]//text()').extract_first()
+            change = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[6]//text()').extract_first()
+            changeperc = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i)+ ']/td[7]//text()').extract_first()
+            volumn = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[8]//text()').extract_first()
+            openinterest = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[9]//text()').extract_first()
+            impliedvolatility = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[1]/div[2]/table/tbody/tr[' + str(i) + ']/td[10]//text()').extract_first()
 
             
             item = YahooFinanceItem() 
@@ -54,3 +53,39 @@ class yahoofinance_spider(scrapy.Spider):
 
 
             yield item
+
+        optiontype = 'put'
+        rows = response.xpath('//table[@class="puts table-bordered W(100%) Pos(r) list-options"]//tr')
+
+
+        for i in range(1, len(rows)):
+
+
+            strike =       rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[1]//text()').extract_first()
+            contractname = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[2]//text()').extract_first()
+            lastprice = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[3]//text()').extract_first()
+            bid = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[4]//text()').extract_first()
+            ask = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[5]//text()').extract_first()
+            change = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[6]//text()').extract_first()
+            changeperc = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[7]//text()').extract_first()
+            volumn = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[8]//text()').extract_first()
+            openinterest = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[9]//text()').extract_first()
+            impliedvolatility = rows[i].xpath('//*[@id="quote-leaf-comp"]/section/section[2]/div[2]/table/tbody/tr[' + str(i) + ']/td[10]//text()').extract_first()
+
+            
+            item = YahooFinanceItem() 
+            item['optiontype'] = optiontype
+            item['strike'] = self.verify(strike)
+            item['contractname'] = self.verify(contractname)
+            item['lastprice'] = self.verify(lastprice)
+            item['bid'] = self.verify(bid)
+            item['ask'] = self.verify(ask)
+            item['change'] = self.verify(change)
+            item['changeperc'] = self.verify(changeperc)
+            item['volumn'] = self.verify(volumn)
+            item['openinterest'] = self.verify(openinterest)
+            item['impliedvolatility'] = self.verify(impliedvolatility)
+
+
+            yield item
+
