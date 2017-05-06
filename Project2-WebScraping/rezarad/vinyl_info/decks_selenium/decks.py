61bf50bea7305714cd66dc9ecd52268a3b4a3a9c
rezaisrad <rezarad77@gmail.com>
2017-05-06 13:34:52 -0400

    removed tracks/genres column from decks scraper

--- Project2-WebScraping/rezarad/vinyl_info/decks_selenium/decks.py
+++ Project2-WebScraping/rezarad/vinyl_info/decks_selenium/decks.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 # create a csv file with data headers
 csv_file = open('decks.csv', 'wb')
 writer = csv.writer(csv_file)
-writer.writerow(['release', 'artist', 'track', 'label_cat', 'genre', 'release_date', 'available'])
+writer.writerow(['release', 'artist', 'label_cat', 'price', 'release_date', 'available'])
 
 # choose a web driver
 driver = webdriver.Chrome()
@@ -32,7 +32,7 @@ while True:
         artists = driver.find_elements_by_xpath('//a[@class = "txt_artist"]')
         # tracks = driver.find_elements_by_xpath('//span[@class = "soundtxt"]/a')
         label_cats = driver.find_elements_by_xpath('//span[@class = "labeltxt"]')
-        genres = driver.find_elements_by_xpath('//a[@class = "txt_styles"]')
+        # genres = driver.find_elements_by_xpath('//a[@class = "txt_styles"]')
         prices = driver.find_elements_by_xpath('//div[@class = "preisschild"]//*')
         release_dates = driver.find_elements_by_xpath('//span[@class = "txt_date"][1]')
         availables = driver.find_elements_by_xpath('//td/form/div/img[1]')
@@ -43,7 +43,7 @@ while True:
             release_info['artist'] = artists[i].text
             # release_info['track'] = tracks[i].text
             release_info['label_cat'] = label_cats[i].text
-            release_info['genre'] = genres[i].text
+            # release_info['genre'] = genres[i].text
             if i % 2 == 0:
                 release_info['price'] = prices[i].text + prices[i + 1].text
             release_info['release_date'] = release_dates[i].text
