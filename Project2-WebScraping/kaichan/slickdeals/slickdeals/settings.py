d28f42ea6dd5ab005cc056814698a5013def08aa
Kai <kaichan91@gmail.com>
2017-05-07 12:57:05 -0400

    uploading spider

---
+++ Project2-WebScraping/kaichan/slickdeals/slickdeals/settings.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for slickdeals project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'slickdeals'
+
+SPIDER_MODULES = ['slickdeals.spiders']
+NEWSPIDER_MODULE = 'slickdeals.spiders'
+
+
+
+
+
+# Cookies Enabled
+COOKIES_ENABLED = True
+
+
+
+
+#AutoLogin MiddleWare
+# DOWNLOADER_MIDDLEWARES = {
+    # 'autologin_middleware.AutologinMiddleware': 305,
+    # 'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': None,
+    # 'autologin_middleware.ExposeCookiesMiddleware': 400,
+# }
+
+# AUTOLOGIN_URL = 'https://slickdeals.net/forums/forumdisplay.php?f=9'
+# AUTOLOGIN_USERNAME= 'anovavona'
+# AUTOLOGIN_PASSWORD= 'Oathkeeper'
+
+# AUTOLOGIN_ENABLED = True
+
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   'slickdeals.pipelines.SlickdealsPipeline': 500,
+   'slickdeals.pipelines.WriteItemPipeline': 600
+}
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'slickdeals (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+#Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 4
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'slickdeals.middlewares.SlickdealsSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'slickdeals.middlewares.MyCustomDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
