18beac4acfb6185201736ecbf11ae05056b6e3cd
Zeyu Zhang <hellozeyu@gmail.com>
2017-05-08 11:13:18 -0400

    code review

--- Project2-WebScraping/rezarad/vinyl_info/data/data_eda.py
+++ Project2-WebScraping/rezarad/vinyl_info/data/data_eda.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+
+# >>>>>> Why not to use ipython notebook for data cleaning? <<<<<<
 deejay = pd.read_csv('./tidy/deejay_tidy.csv')
 decks = pd.read_csv('./tidy/decks_tidy.csv')
 redeye = pd.read_csv('./tidy/redeye_tidy.csv')
