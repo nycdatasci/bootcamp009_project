4e5989800421abd1acdb98cb6612e9bf609d8c01
Mark Schott <m.schott9@gmail.com>
2017-05-17 16:20:36 -0400

    current python EDA, failed random forest, copied kaggle kernels

--- Project3-MachineLearning/aull_dobbins_ganemccalla_schott/schott/EDA/sberbank_EDA.R
+++ Project3-MachineLearning/aull_dobbins_ganemccalla_schott/schott/EDA/sberbank_EDA.R
@@ -1,10 +1,11 @@
-path = ('/data/data/kaggle/sberbank_housing/')
+#path = ('/data/data/kaggle/sberbank_housing/')
+path = '/home/mes/data/kaggle/sberbank_housing/'
 
-base_data = read.csv(paste0(path,'train.csv'))
-dim(base_data) # 30471 x 292
+train = read.csv(paste0(path,'train.csv'))
+dim(train) # 30471 x 292
 macro = read.csv(paste0(path,'macro.csv')) # 2484 x 100
 
-table(sapply(base_data,class))
+table(sapply(train,class))
 #factor integer numeric 
 #16     185      91 
 
@@ -12,12 +13,14 @@ table(sapply(macro,class))
 #factor integer numeric 
 #4      13      83
 
-plot(sapply(base_data, function(x) sum(is.na(x))))
+missing_ratio = sapply(train, function(x) sum(is.na(x))/nrow(train))
+missing_ratio[missing_ratio!=0]
+plot(sapply(train, function(x) sum(is.na(x))/nrow(train)), xlab = 'index of variable', ylab = 'percentage missing')
+plot(sapply(macro, function(x) sum(is.na(x))/nrow(macro)), xlab = 'index of variable', ylab = 'percentage missing')
 
-plot(sapply(macro, function(x) sum(is.na(x))))
 
-plot(base_data[,2],base_data[,292])
-library(plot3D)
-base_data[,2] = as.Date(base_data[,2])
-points3D(base_data[,4],base_data[,292],base_data$children_preschool)
+plot(train[,2],train[,292])
+#library(plot3D)
+train[,2] = as.Date(train[,2])
+#points3D(base_data[,4],base_data[,292],base_data$children_preschool)
 
