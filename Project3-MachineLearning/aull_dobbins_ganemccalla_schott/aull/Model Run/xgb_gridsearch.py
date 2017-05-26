5534f41b633ec7afb93c38262a8e1a866cda5f35
Economic(a) <editor@economica.org>
2017-05-26 07:54:49 -0400

    TGIF

--- Project3-MachineLearning/aull_dobbins_ganemccalla_schott/aull/Model Run/xgb_gridsearch.py
+++ Project3-MachineLearning/aull_dobbins_ganemccalla_schott/aull/Model Run/xgb_gridsearch.py
@@ -287,7 +287,7 @@ print(datetime.now())
 xgb_model = xgb.XGBRegressor()
 opt_GBM = GridSearchCV(xgb_model,gridsearch_params, cv = 5, verbose = 1) 
 opt_GBM.fit(X_train, Y_train)  
-opt_GBM.grid_scores
+opt_GBM.grid_scores_
 opt_GBM.best_model_
 opt_GBM.best_score_
 opt_GBM.best_params_
